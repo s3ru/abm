26cd3ed9f7d095db2011f0330a01e18b8b2873e3
s3ru <sebastian.d.ruf@proton.me>
2025-04-16 22:59:14 +0200

    Add stub for runId processing

--- batch.py
+++ batch.py
@@ -31,18 +31,28 @@ results = mesa.batch_run(
 )
 
 results_df = pd.DataFrame(results)
-print(f"The results have {len(results)} rows.")
-print(f"The columns of the data frame are {list(results_df.keys())}.")
+# print(f"The results have {len(results)} rows.")
+# print(f"The columns of the data frame are {list(results_df.keys())}.")
 # print(results_df.head())
 
 # Iterate over each unique combination of RunId and iteration
 for (run_id, iteration), group_data in results_df.groupby(["RunId", "iteration"]):
 
     sel_columns = ['Step', 'num_agents', 'n_days', 'trading_day', 'market_price', 'info_event', 'true_value', 'bid_ask_spread', 'volume']
-    print(f"Generating visualization for RunId: {run_id}, Iteration: {iteration}")
+    # print(f"Generating visualization for RunId: {run_id}, Iteration: {iteration}")
 
     rows_for_eval = group_data[sel_columns].drop_duplicates()
     rows_for_eval = rows_for_eval.iloc[10:].reset_index(drop=True)
     
     # Create the visualization for the current group
     create_viz(rows_for_eval)
+
+
+run_ids = results_df["RunId"].unique()
+for run_id in run_ids:
+    print(f"Generating visualization for RunId: {run_id}")
+    
+    # Filter the DataFrame for the current RunId
+    run_data = results_df[results_df["RunId"] == run_id]
+    
+    # do calculations for the current run_id
